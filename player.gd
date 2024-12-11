bb07f37e7b3e9459282544129df1a0ef7542c16a
19PettitR <19PettitR@steds.org.uk>
2024-12-11 11:51:06 +0000

    dash smoothed

--- player.gd
+++ player.gd
@@ -4,13 +4,15 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 ## boost from dashing
-const DASH_BOOST = 4
+const DASH_BOOST = 5
 const JUMP_VELOCITY = -400.0
 
 
 ## keeps track of whether the player is dashing or not
 var is_dashing : bool = false
 var dash_cooldown : bool = false
+## track which direction the player was last facing so they can dash in correct direction when idle
+var last_facing_right : bool = true
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
@@ -24,7 +26,7 @@ func _physics_process(delta: float) -> void:
 	
 	## dashing (orange) overrides other colours
 	if not is_dashing:
-		## play the idle animation if on the floor and not moving
+		## play the idle animation only if on the floor and not moving
 		if is_on_floor() and velocity.x == 0:
 			animation_player.play("idle (red)")
 		## play the move animation if on the floor and moving
@@ -47,12 +49,22 @@ func _physics_process(delta: float) -> void:
 		
 		## dashing to true during speed increase
 		is_dashing = true
-		velocity.x = velocity.x * DASH_BOOST
+		
+		## if the player dashes whilst idle, find which direction they were facing
+		if velocity.x == 0:
+			if last_facing_right:
+				velocity.x = SPEED * DASH_BOOST
+			else:
+				velocity.x = SPEED * DASH_BOOST * -1
+		else:
+			velocity.x = velocity.x * DASH_BOOST
+		
+		## start a dash cooldown
+		dash_cooldown = true
+		## dashing for 0.2 seconds
 		await get_tree().create_timer(0.2).timeout
 		is_dashing = false
-		
-		## start a dash cooldown to prevent dashing in quick succession
-		dash_cooldown = true
+		## cool down lasts 1.2 seconds total
 		await get_tree().create_timer(1).timeout
 		dash_cooldown = false
 	
@@ -64,8 +76,17 @@ func _physics_process(delta: float) -> void:
 		if direction:
 			velocity.x = direction * SPEED
 		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+			## find whether the player is dashing or not and decelerate more when they are
+			if is_dashing:
+				velocity.x = move_toward(velocity.x, 0, SPEED)
+			else:
+				velocity.x = move_toward(velocity.x, 0, SPEED * 100)
+	
+	## when a direction key is pressed, track which direction the player is facing for dash
+	if Input.is_action_just_pressed("right"):
+		last_facing_right = true
+	if Input.is_action_just_pressed("left"):
+		last_facing_right = false
 	
 	
-
 	move_and_slide()
